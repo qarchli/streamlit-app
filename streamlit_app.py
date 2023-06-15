6fd890779fd2787db2027c68c3917b0f8d0f9156
Qarchli Mouad <31633271+qarchli@users.noreply.github.com>
2023-06-15 18:14:43 +0200

    prettfy json fruityvice api response into pd dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,10 @@ streamlit.dataframe(fruits_to_show)
 
 # New section for fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# prettify json response to a pd dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# Display dataframe on the page.
+streamlit.dataframe(fruityvice_normalized)
